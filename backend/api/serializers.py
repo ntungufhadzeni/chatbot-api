62df75386965b5e2dac6bc2a43c8c8d2865582d8
Ntungufhadzeni <mbudzenin@thenakedscientists.co.za>
2024-02-01 23:58:42 +0200

    add doc string for documentation

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,4 +2,10 @@ from rest_framework import serializers
 
 
 class ChatSerializer(serializers.Serializer):
+    """
+    Serializer for handling chat-related data.
+
+    Attributes:
+    - text (str): The text content of the chat message.
+    """
     text = serializers.CharField()
