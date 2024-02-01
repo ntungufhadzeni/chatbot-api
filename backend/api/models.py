d34b484684af5d91d66264837be1e19a681012b7
Ntungufhadzeni <mbudzenin@thenakedscientists.co.za>
2024-02-01 12:18:37 +0200

    add models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Step(models.Model):
+    CHOICES = (
+        ('G', 'greeting'),
+        ('Q', 'question'),
+        ('E', 'end'),
+    )
+    name = models.CharField(max_length=1, choices=CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Log(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    step = models.ForeignKey(Step, on_delete=models.CASCADE)
