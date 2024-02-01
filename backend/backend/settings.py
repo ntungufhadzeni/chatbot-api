42dd5f7c02dd0a5381464273f44065d6e1c2523c
Ntungufhadzeni <mbudzenin@thenakedscientists.co.za>
2024-02-01 21:31:42 +0200

    change token expire time

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,11 +61,12 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True
 }
+
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000', ]
 
