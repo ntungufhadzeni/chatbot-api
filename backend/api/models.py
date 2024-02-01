10c239d7d10f7416b24994639ce2a12ba323a97a
Ntungufhadzeni <mbudzenin@thenakedscientists.co.za>
2024-02-01 21:30:19 +0200

    add models

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,11 +8,26 @@ class Step(models.Model):
         ('Q', 'question'),
         ('E', 'end'),
     )
-    name = models.CharField(max_length=1, choices=CHOICES)
+    name = models.CharField(max_length=1, choices=CHOICES, default='G')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    @property
+    def next_step(self):
+        if self.name == 'G':
+            return 'Q'
+        elif self.name == 'Q':
+            return 'E'
+        elif self.name == 'E':
+            return 'G'
 
-class Log(models.Model):
+
+class ChatLog(models.Model):
+    CHOICES = (
+        ('U', 'user'),
+        ('C', 'chat'),
+    )
+    text = models.TextField()
+    sender = models.CharField(max_length=1, choices=CHOICES)
+    step = models.ForeignKey(Step, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    step = models.ForeignKey(Step, on_delete=models.CASCADE)
