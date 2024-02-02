713439214920558c8f0496e6b8329b8a5c8ff5af
Ntungufhadzeni <mbudzenin@thenakedscientists.co.za>
2024-02-02 13:56:51 +0200

    remove serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 
-from .models import Step, Log
-
 
 class ChatSerializer(serializers.Serializer):
     """
@@ -11,37 +9,3 @@ class ChatSerializer(serializers.Serializer):
     - text (str): The text content of the chat message.
     """
     text = serializers.CharField()
-
-
-class StepSerializer(serializers.Serializer):
-    """
-    Serializer for the Step model.
-
-    This serializer is used to convert Step model instances to JSON and vice versa.
-
-    Attributes:
-    - model (Step): The Step model class associated with this serializer.
-    - fields (list): A list of field names to include in the serialized representation.
-
-    """
-
-    class Meta:
-        model = Step
-        fields = '__all__'
-
-
-class LogSerializer(serializers.Serializer):
-    """
-    Serializer for the Log model.
-
-    This serializer is used to convert Log model instances to JSON and vice versa.
-
-    Attributes:
-    - model (Log): The Log model class associated with this serializer.
-    - fields (list): A list of field names to include in the serialized representation.
-
-    """
-
-    class Meta:
-        model = Log
-        fields = '__all__'
